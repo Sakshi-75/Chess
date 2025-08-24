11cf77c1bc7f7e20ff5c401b9ee0a1076dc321f7
Sakshi <sakshi_jain75@gmail.com>
2025-08-24 17:12:48 +0530

    Add more unit tests

---
+++ src/test/java/org/example/utils/BoardUtilsTest.java
@@ -0,0 +1,21 @@
+package org.example.utils;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardUtilsTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"A5", "H8", "B3", "A1", "H1", "A8"})
+    void testValidPosition(String position) {
+        assertTrue(BoardUtils.isValidPositionOnBoard(position));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"A9","Z5", "B0", "I3"})
+    void testInvalidPosition(String position) {
+        assertFalse(BoardUtils.isValidPositionOnBoard(position));
+    }
+}
